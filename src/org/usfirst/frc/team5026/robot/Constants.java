2b3294f786fa52b8567876ae6a034aaa3f171847
James Varah <james@varah.com>
2018-01-16 17:16:35 -0800

    Made some things (J Lee Branch)

--- src/org/usfirst/frc/team5026/robot/Constants.java
+++ src/org/usfirst/frc/team5026/robot/Constants.java
@@ -4,6 +4,9 @@ public class Constants {
 	public static final int RIGHT_PORT = 0;
 	public static final int LEFT_PORT = 1;
 	public static final int FULLSPEED = 1;
+	
+	public static final double SWITCH_POWER = 0.5;
+	public static final double SWITCH_REVERSE_POWER = -0.5;
 	public Constants(){
 		
 	}
--- src/org/usfirst/frc/team5026/robot/OI.java
+++ src/org/usfirst/frc/team5026/robot/OI.java
@@ -1,12 +1,15 @@
 package org.usfirst.frc.team5026.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	public Button driveButton1;
 	public Joystick driveStick;
 	public OI(){
 		driveStick = new Joystick(RobotMap.DRIVE_JOYSTICK);
