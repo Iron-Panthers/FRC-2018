63eca13cea6185a99befa2aebc95c5947cccb23f
Unknown <wesley.larlarb@rookielabs.com>
2018-01-19 16:14:29 -0800

    Auto stash before merge of "autoRobot" and "origin/autoRobot"

--- src/org/usfirst/frc/team5026/robot/Robot.java
+++ src/org/usfirst/frc/team5026/robot/Robot.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc.team5026.robot.subsystems.Drive;
 
 /**
  * The VM is configured to automatically run this class, and to call the
