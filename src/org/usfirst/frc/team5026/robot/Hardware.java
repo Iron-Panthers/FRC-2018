d75919d727431141ef4633a7c7a715bff184e57f
James Varah <james@varah.com>
2018-01-19 16:09:25 -0800

    Control Mode added to Intake

--- src/org/usfirst/frc/team5026/robot/Hardware.java
+++ src/org/usfirst/frc/team5026/robot/Hardware.java
@@ -1,17 +1,22 @@
 package org.usfirst.frc.team5026.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 public class Hardware {
+	public ControlMode controlMode;
 	public TalonSRX rightM;
 	public TalonSRX leftM;
+	public TalonSRX intakeM;
 	public PowerDistributionPanel pdp;
 	public Hardware(){
+		controlMode = ControlMode.PercentOutput;
 		rightM = new TalonSRX(RobotMap.DRIVE_RIGHT_MOTOR);
 		rightM.setInverted(Constants.IS_RIGHT_INVERTED);
 		leftM = new TalonSRX(RobotMap.DRIVE_LEFT_MOTOR);
+		intakeM = new TalonSRX(RobotMap.INTAKE_MOTOR);
 		pdp = new PowerDistributionPanel();
 	}
 }
--- src/org/usfirst/frc/team5026/robot/subsystems/IntakeSubsystem.java
+++ src/org/usfirst/frc/team5026/robot/subsystems/IntakeSubsystem.java
@@ -3,25 +3,29 @@ package org.usfirst.frc.team5026.robot.subsystems;
 import org.usfirst.frc.team5026.robot.Robot;
 import org.usfirst.frc.team5026.robot.commands.IntakeCommand;
 
-import edu.wpi.first.wpilibj.Talon;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class IntakeSubsystem extends Subsystem {
-	public Talon motor;
+	public TalonSRX motor;
+	public ControlMode controlMode;
 	public IntakeSubsystem() {
+		controlMode = Robot.hardware.controlMode;
 		motor = Robot.hardware.intakeM;
 	}
 	public void intake(double speed) {
-		motor.set(speed);
+		motor.set(controlMode, speed);
 	}
 	public void outtake(double speed) {
-		motor.set(speed);
+		motor.set(controlMode, speed);
 	}
 	public void stop() {
-		motor.set(0);
+		motor.set(controlMode, 0);
 	}
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
