4f7b76e25676e679e9a76d5713c72f00fd9c5537
Wesley <wesley.larlarb@rookielabs.com>
2018-02-05 15:19:32 -0800

    Created thresholds for intake power and current
    JAMES GET ON THIS PLZ

--- src/org/usfirst/frc/team5026/robot/Hardware.java
+++ src/org/usfirst/frc/team5026/robot/Hardware.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team5026.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.Talon;
 
 public class Hardware {
@@ -8,7 +9,7 @@ public class Hardware {
 	public Talon leftM;
 	public Talon intakeM;
 	public DigitalInput bannerSensor;
-//	public PowerDistributionPanel pdp;
+	public PowerDistributionPanel pdp;
 	public Hardware(){
 		rightM = new Talon(RobotMap.DRIVE_RIGHT_MOTOR);
 		rightM.setInverted(Constants.IS_RIGHT_INVERTED);
@@ -16,6 +17,6 @@ public class Hardware {
 		intakeM = new Talon(RobotMap.INTAKE_MOTOR);
 		intakeM.setInverted(Constants.IS_INTAKE_INVERTED);
 		bannerSensor = new DigitalInput(0);
-//		pdp = new PowerDistributionPanel();
+		pdp = new PowerDistributionPanel();
 	}
 }
--- src/org/usfirst/frc/team5026/robot/commands/IntakeCommand.java
+++ src/org/usfirst/frc/team5026/robot/commands/IntakeCommand.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team5026.robot.commands;
 
 import org.usfirst.frc.team5026.robot.Constants;
 import org.usfirst.frc.team5026.robot.Robot;
+import org.usfirst.frc.team5026.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -10,7 +11,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class IntakeCommand extends Command {
+	int count;
     public IntakeCommand() {
+    	count = 0;
     	requires(Robot.intake);
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
@@ -29,20 +32,23 @@ public class IntakeCommand extends Command {
     	SmartDashboard.putNumber("magnitude:", Robot.oi.driveStick.getMagnitude());
     	Robot.intake.hasBlock();
 //    	double voltage = Robot.hardware.pdp.getVoltage()*Robot.intake.motor.get();
-//    	double current = Robot.hardware.pdp.getCurrent(RobotMap.INTAKE_PDP_PORT);
+    	double current = Robot.hardware.pdp.getCurrent(RobotMap.INTAKE_PDP_PORT);
 //    	SmartDashboard.putNumber("Intake Current over Voltage", voltage/current);
 //    	SmartDashboard.putNumber("Intake Voltage", voltage);
-//    	SmartDashboard.putNumber("Intake Current", current);
+    	SmartDashboard.putNumber("Intake Current", current);
+    	if(current > Constants.THRESHOLD_CURRENT) {
+    		count++;
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	return count >= Constants.THRESHOLD_COUNT;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.intake.stop();
+    	Robot.intake.intake(Constants.THRESHOLD_POWER);
     }
 
     // Called when another command which requires one or more of the same
