1d48588641e5b0b1561f7d81731a669014afda74
Sc2ad <adamznow@gmail.com>
2018-02-11 13:19:31 -0800

    Removed redundancies

--- src/org/usfirst/frc/team5026/robot/commands/IntakeCommand.java
+++ src/org/usfirst/frc/team5026/robot/commands/IntakeCommand.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team5026.robot.commands;
  
-import org.usfirst.frc.team5026.robot.Constants;
 import org.usfirst.frc.team5026.robot.Robot;
+import org.usfirst.frc.team5026.robot.util.Constants;
 
 import edu.wpi.first.wpilibj.command.Command;
  
