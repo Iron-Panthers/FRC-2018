7732b85f2f9b2d4de5ba6298ec19d90c074fb5c3
Wesley <wesley.larlarb@rookielabs.com>
2018-02-12 22:38:08 -0800

    Forgot one again

--- src/org/usfirst/frc/team5026/robot/util/GoodJoystick.java
+++ src/org/usfirst/frc/team5026/robot/util/GoodJoystick.java
@@ -20,17 +20,23 @@ public class GoodJoystick {
 		double x;
 		x = driveStick.getX();
 		if(Robot.drive.isReversed) {
-			x = -x;
+//			x = -x;
 		}
 		if(Math.abs(x) < Constants.XDEADZONE_SIZE*Math.abs(driveStick.getY())) {
-			x=0;
+			x=0; // bow tie deadzone
 		}
-		else if(Math.sqrt(driveStick.getY()*driveStick.getY() + driveStick.getX()*driveStick.getX()) < Constants.CIRCLE_DEADZONE) {
-			x=0;
+		else if(Math.sqrt(driveStick.getY()*driveStick.getY() + driveStick.getX()*driveStick.getX()) < Constants.CIRCLE_DEADZONE) { 
+			x=0; //circle deadzone
 		}
 		else {
-			x = (x - Constants.YDEADZONE_SIZE);
+			if(x<0) {
+				x = (x + Constants.XDEADZONE_SIZE)/(1-Constants.XDEADZONE_SIZE);
+			}
+			else{
+				x = (x - Constants.XDEADZONE_SIZE)/(1-Constants.XDEADZONE_SIZE);
+			}
 		}
+		SmartDashboard.putNumber("Output X", x);
 		return x;
 	}
 	public double findY() {
@@ -39,27 +45,29 @@ public class GoodJoystick {
 		if(Robot.drive.isReversed) {
 			y = -y;
 		}
-		if(Math.abs(y) < Constants.YDEADZONE_SIZE*Math.abs(driveStick.getX())) {
-			y=0;
+		if(Math.abs(y) < Constants.YDEADZONE_SIZE*Math.abs(driveStick.getX())) { 
+			y=0; //bow tie deadzone
 		}
 		else if(Math.sqrt(driveStick.getY()*driveStick.getY() + driveStick.getX()*driveStick.getX()) < Constants.CIRCLE_DEADZONE) {
-			y=0;
+			y=0; //circle deadzone
 		}
 		else {
-			y = (y - Constants.YDEADZONE_SIZE)/1-Constants.YDEADZONE_SIZE;
+			if(y<0) {
+				y = (y + Constants.YDEADZONE_SIZE)/(1-Constants.YDEADZONE_SIZE);
+			}
+			else {
+				y = (y - Constants.YDEADZONE_SIZE)/(1-Constants.YDEADZONE_SIZE);
+			}
 		}
+		SmartDashboard.putNumber("Output Y", y);
 		return y;
 	}
 	//k = Robot.oi.driveStick.getY()/Robot.oi.driveStick.getX();
 	public double findRightPower(double x,double y) {
-			double right;
-			right = y - x;
-			return right;
+			return y-x;
 	}
 	public double findLeftPower(double x,double y) {
-			double left;
-	        left = y + x;
-	        return left;
+	        return y+x;
 	}
 //	Robot.drive.setLeftMotor(Robot.oi.driveStick.getY() + Robot.oi.driveStick.getX());
 //	Robot.drive.setRightMotor(Robot.oi.driveStick.getY() - Robot.oi.driveStick.getX());
