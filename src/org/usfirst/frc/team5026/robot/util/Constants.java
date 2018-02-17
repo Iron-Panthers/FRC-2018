52032b09e01c1a9043e763f709794ccb526940eb
Sc2ad <adamznow@gmail.com>
2018-02-16 22:20:06 -0800

    Updated speed to match full speed driving
    It was at 0.5 now it is at 1

--- src/org/usfirst/frc/team5026/robot/util/Constants.java
+++ src/org/usfirst/frc/team5026/robot/util/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 	public static final double XDEADZONE_SIZE = SmartDashboard.getNumber("X Deadzone Size", 0.2);
 	public static final double YDEADZONE_SIZE = SmartDashboard.getNumber("Y Deadzone Size", 0.2);
 	public static final double CIRCLE_DEADZONE = SmartDashboard.getNumber("Circle Deadzone Size", 0.2);
-	public static final double SPEED = .5;
+	public static final double SPEED = 1.0;
 	public static final double DEADZONE_SIZE = .1;
 	public static final double QUARTER_SPEED = .4;
 	//Intake and Outtake
