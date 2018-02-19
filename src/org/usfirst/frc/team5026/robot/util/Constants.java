78a60df17c3dec277e4a6f1ac9e204520ab77556
Sc2ad <adamznow@gmail.com>
2018-02-19 13:55:27 -0800

    Fixed Constants using SmartDashboard (redundant)

--- src/org/usfirst/frc/team5026/robot/util/Constants.java
+++ src/org/usfirst/frc/team5026/robot/util/Constants.java
@@ -4,9 +4,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Constants {
 	public static final double X_AXIS_MODIFIER = .8;
-	public static final double XDEADZONE_SIZE = SmartDashboard.getNumber("X Deadzone Size", 0.15);
-	public static final double YDEADZONE_SIZE = SmartDashboard.getNumber("Y Deadzone Size", 0.15);
-	public static final double CIRCLE_DEADZONE = SmartDashboard.getNumber("Circle Deadzone Size", 0.1);
+	public static final double XDEADZONE_SIZE = 0.15;
+	public static final double YDEADZONE_SIZE = 0.15;
+	public static final double CIRCLE_DEADZONE = 0.1;
 	public static final double SPEED = 1.0;
 	public static final double DEADZONE_SIZE = .1;
 	public static final double QUARTER_SPEED = .4;
