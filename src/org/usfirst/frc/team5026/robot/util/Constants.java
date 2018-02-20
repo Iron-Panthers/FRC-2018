3ff94a0b7e5ed9937a117db037a72d499c3764ef
James Varah <james@varah.com>
2018-02-19 17:17:50 -0800

    Added Elevator To Exchange, moved ExtendPiston to Btn12, exchange to 11

--- src/org/usfirst/frc/team5026/robot/util/Constants.java
+++ src/org/usfirst/frc/team5026/robot/util/Constants.java
@@ -32,6 +32,7 @@ public class Constants {
 	public static final double ELEVATOR_F = 0.28;
 	public static final int ELEVATOR_TALL_CUBE_TARGET = 3500;	
 	public static final int ELEVATOR_SHORT_CUBE_TARGET = 2000; //Needs to be tested (This is supposed to lift cube enough so it does not drag on ground)
+	public static final int ELEVATOR_EXCHANGE_TARGET = 3000; //Arbitrary, Needs Testing
 	public static final int ELEVATOR_SWITCH_TARGET = 38000; //Tested by Adam and Driveteam
 	public static final int ELEVATOR_SCALE_TARGET = 85000; //See above
 	public static final int ELEVATOR_TOP_TARGET = 89000; //Tested by James. TODO Needs perfecting!!!
