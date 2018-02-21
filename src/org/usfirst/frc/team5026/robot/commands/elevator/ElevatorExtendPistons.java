73d820f0fae20c57eda0a21da6b621160cb1e9dd
Unknown <serenanh@gmail.com>
2018-02-20 20:50:39 -0800

    Removed interrupted call for retracting the pistons

--- src/org/usfirst/frc/team5026/robot/commands/elevator/ElevatorExtendPistons.java
+++ src/org/usfirst/frc/team5026/robot/commands/elevator/ElevatorExtendPistons.java
@@ -37,6 +37,6 @@ public class ElevatorExtendPistons extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.elevator.retractPistons();
+//    	Robot.elevator.retractPistons();
     }
 }
