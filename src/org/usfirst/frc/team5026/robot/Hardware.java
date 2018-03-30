e6c4a61b40f54c252e942d82afb04f6925c09f7f
LincolnFreedman <21freedmanl96@mybhs.org>
2018-03-29 17:55:53 -0700

    Fixed comments in Hardware

--- src/org/usfirst/frc/team5026/robot/Hardware.java
+++ src/org/usfirst/frc/team5026/robot/Hardware.java
@@ -15,38 +15,38 @@ import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Talon;
 
 public class Hardware {
-//	public TalonSRX elevatorMotor;
-//	public TalonSRX elevatorSlaveMotor;
-	//public ElevatorMotorGroup elevatorMotors;
+	public TalonSRX elevatorMotor;
+	public TalonSRX elevatorSlaveMotor;
+	public ElevatorMotorGroup elevatorMotors;
 	public DoubleSolenoid elevatorSolenoid;
-	/*public TalonSRX conveyor;
+	public TalonSRX conveyor;
 	public TalonSRX intakeM;
 	public TalonSRX rightM1;
 	public TalonSRX rightM2;
 	public TalonSRX rightM3;
 	public TalonSRX leftM1;
 	public TalonSRX leftM2;
-	public TalonSRX leftM3;*/
+	public TalonSRX leftM3;
 	public Talon leftTalonSR;
 	public Talon rightTalonSR;
 	public Spark climbMotor1;
 	public Spark climbMotor2;
-//	public DriveMotorGroup right;
-//	public DriveMotorGroup left;
+	public DriveMotorGroup right;
+	public DriveMotorGroup left;
 	public DoubleSolenoid gearShift;
 	public PowerDistributionPanel pdp;
 	public DigitalInput banner;
 	public DigitalInput elevatorLimit;
 	
 	public Hardware() {
-//		elevatorMotor = new TalonSRX(RobotMap.ELEVATOR_MASTER);
-//		elevatorSlaveMotor = new TalonSRX(RobotMap.ELEVATOR_SLAVE);
-		//elevatorMotors = new ElevatorMotorGroup(elevatorMotor, elevatorSlaveMotor);
-//		elevatorMotor.setInverted(Constants.IS_ELEVATOR_INVERTED);
-//		elevatorSlaveMotor.setInverted(Constants.IS_ELEVATOR_INVERTED);
-//		elevatorMotor.setSensorPhase(!Constants.IS_ELEVATOR_SENSOR_INVERTED);
+		elevatorMotor = new TalonSRX(RobotMap.ELEVATOR_MASTER);
+		elevatorSlaveMotor = new TalonSRX(RobotMap.ELEVATOR_SLAVE);
+		elevatorMotors = new ElevatorMotorGroup(elevatorMotor, elevatorSlaveMotor);
+		elevatorMotor.setInverted(Constants.IS_ELEVATOR_INVERTED);
+		elevatorSlaveMotor.setInverted(Constants.IS_ELEVATOR_INVERTED);
+		elevatorMotor.setSensorPhase(!Constants.IS_ELEVATOR_SENSOR_INVERTED);
 		elevatorSolenoid = new DoubleSolenoid(0, RobotMap.ELEVATOR_SOLENOID_PORT_1, RobotMap.ELEVATOR_SOLENOID_PORT_2);
-		/*conveyor = new TalonSRX(RobotMap.CONVEYOR_BELT_MOTOR);
+		conveyor = new TalonSRX(RobotMap.CONVEYOR_BELT_MOTOR);
 		conveyor.setInverted(Constants.IS_COVEYOR_INVERTED);
 		conveyor.setNeutralMode(NeutralMode.Brake);
 		intakeM = new TalonSRX(RobotMap.INTAKE_MOTOR);
@@ -58,18 +58,18 @@ public class Hardware {
 		leftM3 = new TalonSRX(RobotMap.DRIVE_LEFT_MOTOR_3);
 		right = new DriveMotorGroup(rightM1, rightM2, rightM3);
 		left = new DriveMotorGroup(leftM1, leftM2, leftM3);
-		rightM1.configOpenloopRamp(Constants.RIGHTM_RAMP_RATE, Constants.kTimeoutMs);
-		leftM1.configOpenloopRamp(Constants.LEFTM_RAMP_RATE, Constants.kTimeoutMs);*/
+//		rightM1.configOpenloopRamp(Constants.RIGHTM_RAMP_RATE, Constants.kTimeoutMs);
+//		leftM1.configOpenloopRamp(Constants.LEFTM_RAMP_RATE, Constants.kTimeoutMs);
 		leftTalonSR = new Talon(RobotMap.TALON_SR_DRIVE_LEFT_MOTOR);
 		leftTalonSR.setInverted(true);
 		climbMotor1 = new Spark(0);
 		climbMotor2 = new Spark(1);
 		rightTalonSR = new Talon(RobotMap.TALON_SR_DRIVE_RIGHT_MOTOR);
-//		right.setInverted(Constants.IS_RIGHT_INVERTED);
-//		left.setInverted(Constants.IS_LEFT_INVERTED);
-//		left.setSensorInverted(Constants.IS_LEFT_SENSOR_INVERTED);
-//		right.setSensorInverted(Constants.IS_RIGHT_SENSOR_INVERTED);
-		//intakeM.setInverted(Constants.IS_INTAKE_INVERTED);
+		right.setInverted(Constants.IS_RIGHT_INVERTED);
+		left.setInverted(Constants.IS_LEFT_INVERTED);
+		left.setSensorInverted(Constants.IS_LEFT_SENSOR_INVERTED);
+		right.setSensorInverted(Constants.IS_RIGHT_SENSOR_INVERTED);
+		intakeM.setInverted(Constants.IS_INTAKE_INVERTED);
 		gearShift = new DoubleSolenoid(0, RobotMap.SHIFT_FORWARD, RobotMap.SHIFT_REVERSE);
 		pdp = new PowerDistributionPanel();
 		banner = new DigitalInput(RobotMap.BANNER_PORT);
