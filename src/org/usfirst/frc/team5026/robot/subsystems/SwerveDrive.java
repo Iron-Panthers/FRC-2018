02fca371198263dedaaf2dad3826a5a607a83279
Wesley <wesley.larlarb@rookielabs.com>
2018-05-02 15:25:15 -0700

    sine boi

--- src/org/usfirst/frc/team5026/robot/subsystems/SwerveDrive.java
+++ src/org/usfirst/frc/team5026/robot/subsystems/SwerveDrive.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team5026.robot.subsystems;
 
 import org.usfirst.frc.team5026.robot.commands.SwerveDriveCommand;
+import org.usfirst.frc.team5026.util.Constants;
 import org.usfirst.frc.team5026.util.SwerveMotorGroup;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -10,16 +11,16 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class SwerveDrive extends Subsystem {
 	public SwerveMotorGroup swerveMotors;
-	DriveMotorGroup left;
-	DriveMotorGroup right;
-	public SwerveDrive(SwerveMotorGroup swerveMotors, DriveMotorGroup left, DriveMotorGroup right) {
+	DiagonalMotorGroup left;
+	DiagonalMotorGroup right;
+	public SwerveDrive(SwerveMotorGroup swerveMotors, DiagonalMotorGroup left, DiagonalMotorGroup right) {
 		this.swerveMotors = swerveMotors;
 		this.left = left;
 		this.right = right;
 	}
 	public void setLeftRightMotors(double adjustedZ, double adjustedMagnitude) {
-		left.set(adjustedMagnitude+adjustedZ);
-		right.set(adjustedMagnitude-adjustedZ);
+		left.set(adjustedMagnitude + adjustedZ*Math.sin(2*(swerveMotors.m1.getSelectedSensorPosition(pidIdx)/Constants.ENCODER_TICKS_PER_REVOLUTION)%1) - (Math.PI/4));
+		right.set(adjustedMagnitude - adjustedZ*Math.sin(2*(swerveMotors.m1.getSelectedSensorPosition(pidIdx)/Constants.ENCODER_TICKS_PER_REVOLUTION)%1) + (Math.PI/4));
 	}
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
--- src/org/usfirst/frc/team5026/util/SwerveMotorGroup.java
+++ src/org/usfirst/frc/team5026/util/SwerveMotorGroup.java
@@ -4,7 +4,7 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class SwerveMotorGroup {
-	TalonSRX m1;
+	public TalonSRX m1;
 	TalonSRX m2;
 	TalonSRX m3;
 	TalonSRX m4;
